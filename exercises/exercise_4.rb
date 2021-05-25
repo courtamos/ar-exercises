74568579eb04316cd43d9eb1706bd8380a27a7f0
Courtney Amos <c.amos@shaw.ca>
2021-05-24 21:02:13 -0700

    exercise 4: laoding a subset of stores

--- exercises/exercise_4.rb
+++ exercises/exercise_4.rb
@@ -7,3 +7,16 @@ puts "Exercise 4"
 puts "----------"
 
 # Your code goes here ...
+Store.create(name: 'Surrey', annual_revenue: 224000, mens_apparel: false, womens_apparel: true)
+Store.create(name: 'Whistler', annual_revenue: 1900000, mens_apparel: true, womens_apparel: false)
+Store.create(name: 'Yaletown', annual_revenue: 430000, mens_apparel: true, womens_apparel: true)
+
+@mens_stores = Store.where(mens_apparel: true)
+p @mens_stores
+
+p @mens_stores.map { |store| puts "#{store.name}: $#{store.annual_revenue}"}
+
+@womens_stores = Store.where("womens_apparel = true and annual_revenue < 1000000")
+p @womens_stores
+
+@womens_stores.map { |store| puts "#{store.name}: $#{store.annual_revenue}"}
