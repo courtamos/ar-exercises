790435a72c47a7814b518757e7994a435216c064
Courtney Amos <c.amos@shaw.ca>
2021-05-24 17:39:39 -0700

    exercise 2: update the first store

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -5,3 +5,12 @@ puts "Exercise 2"
 puts "----------"
 
 # Your code goes here ...
+
+@store1 = Store.first
+p @store1
+
+@store2 = Store.find_by id: 2
+p @store2
+
+@store1.name = "New Name"
+p @store1
